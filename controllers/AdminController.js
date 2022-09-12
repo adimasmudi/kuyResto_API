026160b9a6a203eb1af0b8edfdee2f08c4f9e34c
adimasmudi <adimasmudi@gmail.com>
2022-09-12 14:36:07 +0700

    add some display

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -9,14 +9,29 @@ module.exports = {
       page: "customer",
     });
   },
+  displayCustomerDetail: async (req, res) => {
+    res.render("index", {
+      page: "customerDetail",
+    });
+  },
   displayMenu: async (req, res) => {
     res.render("index", {
       page: "menu",
     });
   },
+  displayAddMenu: async (req, res) => {
+    res.render("index", {
+      page: "addMenu",
+    });
+  },
   displayTransaction: async (req, res) => {
     res.render("index", {
       page: "transaction",
+    });
+  },
+  displayCart: async (req, res) => {
+    res.render("index", {
+      page: "cart",
     });
   },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,10 @@ const AdminController = require("../controllers/AdminController");
 
 router.get("/", AdminController.index);
 router.get("/customer", AdminController.displayCustomer);
+router.get("/customer/:id", AdminController.displayCustomerDetail);
 router.get("/menu", AdminController.displayMenu);
+router.get("/menu/add", AdminController.displayAddMenu);
 router.get("/transaction", AdminController.displayTransaction);
+router.get("/cart", AdminController.displayCart);
 
 module.exports = router;
