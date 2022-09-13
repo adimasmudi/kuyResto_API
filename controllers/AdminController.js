111ffe9ff900c6186ddc8df38ae05ec24a68a9c1
adimasmudi <adimasmudi@gmail.com>
2022-09-13 12:54:33 +0700

    repair admin UI

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -17,6 +17,39 @@ module.exports = {
   displayMenu: async (req, res) => {
     res.render("index", {
       page: "menu",
+      menus: [
+        {
+          id: 1,
+          name: "bakso beranak",
+          category: "makanan",
+          description:
+            "Makanan yang sangat enak disantap di waktu dingin bersama orang tersayang",
+          price: 20000,
+          discount: 5000,
+          qty: 4,
+          image: "/images/bakso_beranak.jpg",
+        },
+        {
+          id: 5,
+          name: "Lemon Tea",
+          category: "minuman",
+          description: "Lemonnya bener-bener terasa",
+          price: 7000,
+          discount: 1000,
+          qty: 5,
+          image: "/images/lemon_tea.jpg",
+        },
+        {
+          id: 6,
+          name: "Somay",
+          category: "camilan",
+          description: "wenak tenan",
+          price: 5000,
+          discount: 0,
+          qty: 5,
+          image: "/images/somay.jpg",
+        },
+      ],
     });
   },
   displayAddMenu: async (req, res) => {
